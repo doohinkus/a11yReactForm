685ea8822db6752d7e64ef1947b01110037fef3b
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2020-10-11 19:58:21 -0700

    add state helpers

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import { useStateValue } from '../state/state.provider';
-import { updateFieldValue, addFieldError, clearFieldError } from '../state/form.duck';
+import { handleBlur } from  '../state/state.helpers';
+import { updateFieldValue } from '../state/form.duck';
 import Error from './Error';
 
 export default function Input({...props}){
-    function handleBlur({fieldValues, validate, errorMessage}){
-      console.log("handleblur", typeof validate, " ", fieldValues, errorMessage)
-      if(fieldValues && fieldValues.value){
-        // console.log(props.validate(fieldValues.value), "field ", fieldValues.value)
-        if(props.validate(fieldValues.value)){
-          // dispatch error
-          return dispatch(addFieldError(
-              {
-                id: fieldValues.id, 
-                errorMessage: errorMessage || "default error message"
-              })
-            );
-        }
-        // clear error
-        return dispatch(clearFieldError({id: fieldValues.id}));
-      }
-      return
-    }
     const [{ fields }, dispatch] = useStateValue();
     console.log(fields)
     const fieldValues = fields.filter((field) => field.id === props.id)[0];
@@ -33,7 +16,13 @@ export default function Input({...props}){
         <input 
           tabIndex={0}
           onChange={e => dispatch(updateFieldValue(e))}
-          onBlur={() => handleBlur({fieldValues, validate: props.validate, errorMessage: props.errorMessage})}
+          onBlur={() => handleBlur({ 
+              dispatch,
+              fieldValues, 
+              validate: props.validate, 
+              errorMessage: props.errorMessage
+            
+          })}
           {...props}
         />
           <p> 
---
+++ src/state/state.helpers.js
@@ -0,0 +1,16 @@
+import { addFieldError, clearFieldError } from '../state/form.duck';
+
+export function handleBlur({fieldValues, errorMessage, validate, dispatch}){
+  if(fieldValues && fieldValues.value){
+    if(validate(fieldValues.value)){
+      return dispatch(addFieldError(
+          {
+            id: fieldValues.id, 
+            errorMessage: errorMessage || "default error message"
+          })
+        );
+    }
+    return dispatch(clearFieldError({id: fieldValues.id}));
+  }
+  return
+}
