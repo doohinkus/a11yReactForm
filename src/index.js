aa2bb1219633c4611390f773187bd54dac0751fa
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2020-10-22 07:59:20 -0700

    add react context tool for debugging contenxt

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { debugContextDevtool } from 'react-context-devtool';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { initialState, formReducer } from "./state/form.duck";
 import { StateProvider } from "./state/state.provider";
 
+const container = document.getElementById('root');
+
 ReactDOM.render(
   <StateProvider initialState={initialState} reducer={formReducer}>
     <App />
   </StateProvider>,
-  document.getElementById('root')
+  container
 );
-
+debugContextDevtool(container);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
